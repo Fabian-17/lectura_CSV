d05c3fc0909eb31b374a81c6836fb80b2a05c0b0
Fabian Villagra <fabianvillagra28@gmail.com>
2024-04-10 14:19:13 -0300

    feat: creacion de la tabla en la base de datos

--- app.py
+++ app.py
@@ -1,25 +1,49 @@
 import csv 
 import MySQLdb as mysql
 
+try:
+    db = mysql.connect('localhost', 'root', '', 'tp')
+except mysql.Error as e:
+    print(f"Error al conectar a MySQL: {e}")
+    exit(1)
+
+
 with open('localidades.csv', newline='') as archivo_csv:
     lector_csv = csv.reader(archivo_csv, delimiter=',', quotechar='"')
     for fila in lector_csv:
         print(fila)
 
 
-# se exporta los datos a una base de datos mysql 
-
 try:
-    db = mysql.connect('localhost', 'root', '', 'localidades')
+    cursor = db.cursor()
+
+    # Crear la tabla 'localidades' si no existe
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS localidades (
+            id INT,
+            provincia VARCHAR(255) NOT NULL,
+            localidad VARCHAR(255) NOT NULL,
+            cp VARCHAR(10) NOT NULL,
+            id_prov_mstr INT
+        )
+    """)
+
 except mysql.Error as e:
-    print(f"Error al conectar a MySQL: {e}")
+    print(f"Error al crear la tabla 'localidades' en MySQL: {e}")
     exit(1)
 
-cursor = db.cursor()
-sql = "INSERT INTO localidades (id, nombre, provincia) VALUES (%s, %s, %s)" 
+
 try:
-    cursor.executemany(sql, lector_csv)
-    db.commit()
-except:
+    cursor = db.cursor()
+    cursor.execute("SELECT  DISTINCT provincia FROM localidades;")
+    provincias =cursor.fetchall()
+    for provincia in provincias:
+        cursor.execute("SELECT * FROM localidades WHERE provincia = %s", (provincia[0], ))
+        localidades = cursor.fetchall()
+        with open(f"csv/{provincia[0]}.csv", "w", newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(localidades)
+except mysql.Error as e:
     db.rollback()
-db.close()
+    print(f"Error al obtener los datos de MySQL: {e}")
+    exit(1)
